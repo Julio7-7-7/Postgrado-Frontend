ab0a750da31c8b7c189d74b45ddbc0f269797dc2
Julio7-7-7 <julio.toledo2030@gmail.com>
2026-05-15 16:12:50 -0400

    feat: mejorando la logica y estilos de el tipo programa list

--- src/app/core/config/app.routes.ts
+++ src/app/core/config/app.routes.ts
@@ -2,12 +2,16 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    loadComponent: () =>
+      import('../../features/home/pages/home').then(m => m.HomeComponent)
+  },
+  {
     path: 'tipos-programa',
     loadChildren: () =>
-      import('../../features/tipo-programa/routes/tipo-programa.routes') .then(m => m.TIPO_PROGRAMA_ROUTES)
+      import('../../features/tipo-programa/routes/tipo-programa.routes')
+      .then(m => m.TIPO_PROGRAMA_ROUTES)
   },
-  // Redirección por defecto: si la URL está vacía, nos manda a tipos-programa
-  { path: '', redirectTo: 'tipos-programa', pathMatch: 'full' },
-  // Si escriben cualquier otra cosa que no existe, también los mandamos a la lista
-  { path: '**', redirectTo: 'tipos-programa' }
+  { path: '**', redirectTo: '' }
 ];
+
